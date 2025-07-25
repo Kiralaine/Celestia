64761029229a5ca081672103b9e1380beaeff95f
Kiralaine <k9034449@gmail.com>
2025-07-25 20:39:57 +0500

    2nd layer done

---
+++ src/Celestia.Repository/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Celestia.DataAccess.Entities;
+
+namespace SkillSystem.Repository.Repositories;
+
+public interface IUserRepository
+{
+    Task<long> InsertAsync(UserEntity user);
+    Task<ICollection<UserEntity>> SelectAllAsync();
+
+    Task<UserEntity?> SelectByIdAsync(long userId);
+
+    Task<bool> CheckUserExistance(long userId);
+    Task DeleteAsync(UserEntity user);
+    Task UpdateAsync(UserEntity user);
+}
---
+++ src/Celestia.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using Celestia.DataAccess;
+using Celestia.DataAccess.Entities;
+using Celestia.DataAcess;
+using Microsoft.EntityFrameworkCore;
+
+namespace SkillSystem.Repository.Repositories;
+public class UserRepository : IUserRepository
+{
+    private readonly MainContext MainContext;
+    public UserRepository(MainContext mainContext)
+    {
+        MainContext = mainContext;
+    }
+    
+    public async Task<bool> CheckUserExistance(long userId)
+    {
+        return await MainContext.Users.AnyAsync(x => x.UserId == userId);
+    }
+
+    public async Task DeleteAsync(UserEntity user)
+    {
+        MainContext.Users.Remove(user);
+        await MainContext.SaveChangesAsync();
+    }
+
+    public async Task<long> InsertAsync(UserEntity user)
+    {
+        await MainContext.Users.AddAsync(user);
+        await MainContext.SaveChangesAsync();
+        return user.UserId;
+    }
+
+    public async Task<ICollection<UserEntity>> SelectAllAsync()
+    {
+        var users = await MainContext.Users.ToListAsync();
+
+        return users;
+    }
+
+    public async Task<UserEntity?> SelectByIdAsync(long userId)
+    {
+        var user = await MainContext.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+
+        if(user == null)
+        {
+            return user;
+        }
+
+        return user;
+    }
+
+    public async  Task UpdateAsync(UserEntity user)
+    {
+        MainContext.Users.Update(user);
+        await MainContext.SaveChangesAsync();
+
+    }
+}
